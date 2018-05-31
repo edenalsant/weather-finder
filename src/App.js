da811bca2711b9b3a63190d25a076a7d04ca8e80
Eden Ernandes <eeas@cin.ufpe.br>
2018-05-31 11:52:02 -0300

    css styles and bootstrap

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ class App extends React.Component{
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json(); //converts whats comes from the fetch function into json
+  
+   
     if(city && country){
       console.log(data);
       this.setState({
@@ -43,20 +45,30 @@ class App extends React.Component{
 
   render(){
     return (
-      <div>
-        <Titles />
-        <Form getWeather={this.getWeather} />
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          description={this.state.description}
-          error={this.state.error}
-        />
-        
+      <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles/>
+                </div>
+                <div className="col-xs-7 form-container">
+                <Form getWeather={this.getWeather} />
+                <Weather 
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  description={this.state.description}
+                  error={this.state.error}
+                />
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 };
 
 export default App;
+
